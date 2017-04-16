1604712c0aa65f84ff89662b46390d2105f5a866
Owen Phillips <ophilli@clemson.edu>
2017-04-15 21:21:17 -0400

    Correct Bus preventCache Timestamp

--- src/my.clemson.py
+++ src/my.clemson.py
@@ -3,15 +3,12 @@ import json
 import time
 
 with requests.Session() as s:
-	parameters = {'request.preventCache' : time.time() * 1000};
-
-	x = 0
 	while(True):	
+		parameters = {'request.preventCache' : (int)(time.time() * 1000)}
+		print(parameters)
+
 		r = s.get('https://my.clemson.edu/srv/feed/static/buses/positions.json', params = parameters)
 		
 		print(json.dumps(r.json()[4], indent=2))
-		
-		x += 1
-		print(x)
 
 		time.sleep(1)
